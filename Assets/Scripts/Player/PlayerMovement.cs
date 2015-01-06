62641ed6e83141d1e78e66f12c16a79bbdb8b693
Paradoxium <anmcmill@ucsc.edu>
2015-01-06 00:41:57 -0800

    First commit of the survival shooter tutorial. Has player and camera movement.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	//public varibables first
+	public float speed = 6f;
+
+	//then private
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	//sets up refrences
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor"); //floor is the name of the layer
+		anim = GetComponent <Animator> (); //FuncitonName<type>();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+	//unity automatically calls all the physics updates with this
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal"); //Default axis that maps to A&D or <- & ->
+		float v = Input.GetAxisRaw ("Vertical"); 
+
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f, v); //latteral movment, no need to go up and down
+
+		movement = movement.normalized * speed * Time.deltaTime; //normalizes so it moves at the same speed in any direction you move. Per Second with Time.deltaTime
+
+		playerRigidbody.MovePosition (transform.position + movement); //Actualy moves the character relative to its previous position.
+
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition); //finds the point under the mouse
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) //floorMask makes use it only pulls info from that layer.
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse); //Quaternion is a type that stores rotation data, in this case the direction of the mouse from the player is forward
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f; //did either of these get triggered
+		anim.SetBool ("IsWalking", walking);
+	}
+
+}
---
+++ Assets/ballcontrol.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+//these are now properties of the object  in game and can be edited in unity
+//thses are public variables that show up in unity
+var rotationSpeed = 1000;
+var jumpHeight = 8;
+
+
+//privates are only accessable within the script.
+private var isFalling = false;
+
+function Update () {
+	//this is the code for horizontal movement.
+	var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed;
+	rotation *= Time.deltaTime;
+	rigidbody.AddRelativeTorque(Vector3.back * rotation);
+	if (Input.GetKeyDown(KeyCode.W) && isFalling == false)
+	{
+		rigidbody.velocity.y = jumpHeight;
+		
+	}
+	isFalling = true;
+}
+
+function OnCollisionStay ()
+{
+	isFalling = false;
+
+
+}
